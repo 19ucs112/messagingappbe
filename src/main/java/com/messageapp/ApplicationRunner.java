02217bdefaf8262ac4df93392289dd0bcf37493a
shaik <ishaik5409@gmail.com>
2023-01-08 14:51:35 +0530

    add swagger support for api's

--- src/main/java/com/messageapp/ApplicationRunner.java
+++ src/main/java/com/messageapp/ApplicationRunner.java
@@ -3,11 +3,17 @@ package com.messageapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.messageapp.model")
 public class ApplicationRunner {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationRunner.class, args);
+    }
+
+    public void handleApplicationStartEvent(ServletWebServerInitializedEvent event){
+        int applicationRunningPort = event.getWebServer().getPort();
+        System.out.println("use swagger url: http://localhost:"+applicationRunningPort+"/swagger-ui.html");
     }
 }
